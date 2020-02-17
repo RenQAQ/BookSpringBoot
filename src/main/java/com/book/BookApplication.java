14e94b3ccee2259dafeda135dc17d0f2eb88e747
RenQAQ <596938857@qq.com>
2020-02-17 17:16:40 +0800

    Revert "Revert "Revert "BookSpringBoot""" 回滚

--- src/main/java/com/book/BookApplication.java
+++ src/main/java/com/book/BookApplication.java
@@ -8,7 +8,6 @@ public class BookApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BookApplication.class, args);
-        System.out.println("张兴蠢B");
     }
 
 }
